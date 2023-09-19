69e68eb53c1b90cfc21afecc7321233befde648a
NaKyeongLee <93971623+NaKyeongLee@users.noreply.github.com>
2023-09-19 11:46:00 +0900

    study

---
+++ blue/Chapter01/1st_week/1st_week/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1st_week
+//
+//  Created by user on 2023/09/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
